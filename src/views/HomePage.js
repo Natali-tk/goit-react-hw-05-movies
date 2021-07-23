4b93319732b0ee7b1643a31c5de847a25de00e5c
Nataly <natasha.totska@yahoo.com>
2021-07-23 20:01:29 +0300

    Add

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { fetchTrandingMovies } from '../services/movies-api';
-import s from '../components/MovieDetailsPage/Movies.module.css';
+import s from '../components/MoviePageDetails/MoviePageDetails.module.css';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
